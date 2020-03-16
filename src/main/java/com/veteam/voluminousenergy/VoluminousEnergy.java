9527e8ee2ec9b4f9c5f3b097cad50c5812300d42
MikeTheShadow <miketheshadow1@gmail.com>
2020-03-16 00:06:11 -0600

    Init mod

--- src/main/java/com/veteam/voluminousenergy/VoluminousEnergy.java
+++ src/main/java/com/veteam/voluminousenergy/VoluminousEnergy.java
@@ -1,6 +1,42 @@
 package com.veteam.voluminousenergy;
 
+import net.minecraft.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("voluminousenergy")
 public class VoluminousEnergy
 {
 
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public VoluminousEnergy() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        LOGGER.info("Hello from server start!");
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegisterEvents {
+
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegisteryEvent){
+            LOGGER.info("Hello from block registry!");
+        }
+    }
 }
