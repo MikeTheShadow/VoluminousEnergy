b86c319d3952704bd9289e3ea415bda997cc272e
Eelt <davidbuddy9@gmail.com>
2020-03-17 16:24:14 -0400

    Tweaks to Saltpeter Ore

--- src/main/java/com/veteam/voluminousenergy/blocks/SaltpeterOre.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/SaltpeterOre.java
@@ -1,15 +1,16 @@
 package com.veteam.voluminousenergy.blocks;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.FallingBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
-public class SaltpeterOre extends Block {
+public class SaltpeterOre extends FallingBlock {
     public SaltpeterOre(){
         super(Properties.create(Material.SAND)
             .sound(SoundType.SAND)
             .hardnessAndResistance(0.6f)
         );
+
         setRegistryName("saltpeterore");
     }
 }
