a57d0d191c8bab009146861c35c834273c7e0a36
MikeTheShadow <miketheshadow1@gmail.com>
2020-03-18 16:45:57 -0600

    Fixed ore gen

--- src/main/java/com/veteam/voluminousenergy/world/VEOreGeneration.java
+++ src/main/java/com/veteam/voluminousenergy/world/VEOreGeneration.java
@@ -1,6 +1,11 @@
 package com.veteam.voluminousenergy.world;
 
+import com.veteam.voluminousenergy.VoluminousEnergy;
 import com.veteam.voluminousenergy.blocks.VEBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.pattern.BlockMatcher;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.GenerationStage;
@@ -8,7 +13,12 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.common.IExtensibleEnum;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import javax.sql.rowset.Predicate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static net.minecraft.world.biome.Biome.LOGGER;
 
@@ -17,18 +27,40 @@ public class VEOreGeneration {
     public static void OreGeneration(){
         //Setup CountRangeConfigs here for each ore
         CountRangeConfig saltpeterOreConf = new CountRangeConfig(175, 55, 0, 256);//"Weight", Minimum Height, topOffset (Adjusts from max value), Maximum height
+        List<Block> sandList = new ArrayList<>();
+        sandList.add(Blocks.SAND);
+        sandList.add(Blocks.RED_SAND);
+
+        List<Block> stoneList = new ArrayList<>();
+        stoneList.add(Blocks.STONE);
+        stoneList.add(Blocks.GRANITE);
+        stoneList.add(Blocks.DIORITE);
+        stoneList.add(Blocks.ANDESITE);
+
+        List<Block> overWorld = new ArrayList<>();
+        overWorld.addAll(stoneList);
+        overWorld.addAll(sandList);
 
         //Run through every single biome registered with Forge
         for (Biome biome : ForgeRegistries.BIOMES){
-
             //If category is Desert. Desert, Desert Lakes, and Desert Hills included to ENSURE this will work (may be unecessary)
             if (biome.getCategory() == Biome.Category.DESERT || biome == Biomes.DESERT || biome == Biomes.DESERT_LAKES || biome == Biomes.DESERT_HILLS){
                 //Generate Saltpeter ore in the sand LOGGER.info("Saltpeter ore has been registered!");
-                //TODO: Make Saltpeter generate in sand, not stone
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, VEBlocks.SALTPETER_ORE.getDefaultState(),33), Placement.COUNT_RANGE,saltpeterOreConf));
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, configBuilder("SAND_BLOCK","SAND",sandList,33,VEBlocks.SALTPETER_ORE.getDefaultState()), Placement.COUNT_RANGE,saltpeterOreConf));
             }
 
         }
 
     }
+    private static OreFeatureConfig configBuilder(String name,String sName,List<Block> blockType,int size,BlockState state) {
+        return new OreFeatureConfig(OreFeatureConfig.FillerBlockType.create(name,sName,(BlockState) -> {
+            if (BlockState == null) {
+                return false;
+            } else {
+                Block block = BlockState.getBlock();
+                return blockType.contains(block);
+            }
+        }), state,size);
+    }
+
 }
