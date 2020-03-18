04ae5d4b91b2a9cd69602beb905182b4146396fc
Eelt <davidbuddy9@gmail.com>
2020-03-18 15:25:04 -0400

    Added Stirling, fixed particles, other enhancements
    * Added Primitive Stirling Generator
    * Set particle effects to minecraft:block/iron_block
    *Added appropriate tool types to Saltpeter Ore, Primitive Blast Furnace, Primitive Stirling Generator

--- src/main/java/com/veteam/voluminousenergy/blocks/SaltpeterOre.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/SaltpeterOre.java
@@ -3,14 +3,16 @@ package com.veteam.voluminousenergy.blocks;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ShovelItem;
+import net.minecraftforge.common.ToolType;
 
 public class SaltpeterOre extends FallingBlock {
     public SaltpeterOre(){
         super(Properties.create(Material.SAND)
             .sound(SoundType.SAND)
             .hardnessAndResistance(0.6f)
+            .harvestTool(ToolType.SHOVEL)
         );
-
         setRegistryName("saltpeterore");
     }
 }
