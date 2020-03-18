7891f2d1192ff5b7cff742cf295fd55de4b8462d
MikeTheShadow <miketheshadow1@gmail.com>
2020-03-18 02:37:29 -0600

    Implemented FaceableBlock.java extend this when making a block that needs to face a certain way

--- src/main/java/com/veteam/voluminousenergy/blocks/FaceableBlock.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/FaceableBlock.java
@@ -4,19 +4,8 @@ package com.veteam.voluminousenergy.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DirectionalBlock;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemStack;
-import net.minecraft.state.IProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.world.World;
-
-import javax.annotation.Nullable;
 
 public class FaceableBlock extends DirectionalBlock
 {
@@ -24,17 +13,6 @@ public class FaceableBlock extends DirectionalBlock
     public FaceableBlock(Properties properties)
     {
         super(properties);
-    }
-
-    @Override
-    public boolean hasTileEntity() {
-        return true;
-    }
-
-    @Nullable
-    @Override
-    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-        return new PrimitiveBlastFurnaceTile();
     }
 
     @Override
