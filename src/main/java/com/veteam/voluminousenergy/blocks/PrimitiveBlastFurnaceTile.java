c5375bc24db8afcd4adaa14a6ef1e5ef594c2cbc
MikeTheShadow <miketheshadow1@gmail.com>
2020-03-18 18:53:23 -0600

    Better caching with Tile Entities

--- src/main/java/com/veteam/voluminousenergy/blocks/PrimitiveBlastFurnaceTile.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/PrimitiveBlastFurnaceTile.java
@@ -1,56 +1,75 @@
 package com.veteam.voluminousenergy.blocks;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 
 public class PrimitiveBlastFurnaceTile extends TileEntity implements ITickableTileEntity {
 
-    private ItemStackHandler handler;
+    private LazyOptional handler = LazyOptional.of(this::createHandler).cast();
+
 
     public PrimitiveBlastFurnaceTile() {
-        super(VEBlocks.PRIMITIVE_BLAST_FURNACE_TILE);
+        super(VEBlocks.PRIMITIVE_STIRLING_GENERATOR_TILE);
     }
 
     @Override
     public void tick() {
-
     }
 
     @Override
     public void read(CompoundNBT tag) {
         CompoundNBT inv = tag.getCompound("inv");
-        getHandler().deserializeNBT(inv);
+        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>)h).deserializeNBT(inv));
+        createHandler().deserializeNBT(inv);
         super.read(tag);
     }
 
     @Override
     public CompoundNBT write(CompoundNBT tag) {
-        CompoundNBT compound = getHandler().serializeNBT();
-        tag.put("inv",compound);
+        handler.ifPresent(h -> {
+            CompoundNBT compound = ((INBTSerializable<CompoundNBT>)h).serializeNBT();
+            tag.put("inv",compound);
+        });
         return super.write(tag);
     }
 
-    private ItemStackHandler getHandler() {
-        if(handler == null) {
-            handler = new ItemStackHandler(1);
-        }
-        return handler;
+    private ItemStackHandler createHandler() {
+        return new ItemStackHandler(1) {
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                return stack.getItem() == Items.DIAMOND;
+            }
+
+            @Nonnull
+            @Override
+            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate)
+            {
+                if(stack.getItem() != Items.DIAMOND) {
+                    return stack;
+                }
+                return super.insertItem(slot, stack, simulate);
+            }
+        };
     }
 
     @Nonnull
     @Override
-    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, Direction side) {
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
         if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return LazyOptional.of(()-> (T) getHandler());
+            return handler.cast();
         }
         return super.getCapability(cap, side);
     }
--- src/main/java/com/veteam/voluminousenergy/blocks/PrimitiveStirlingGeneratorTile.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/PrimitiveStirlingGeneratorTile.java
@@ -1,11 +1,15 @@
 package com.veteam.voluminousenergy.blocks;
 
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
@@ -16,7 +20,8 @@ import javax.annotation.Nullable;
 public class PrimitiveStirlingGeneratorTile extends TileEntity implements ITickableTileEntity
 {
 
-    private ItemStackHandler handler;
+    private LazyOptional handler = LazyOptional.of(this::createHandler).cast();
+
 
     public PrimitiveStirlingGeneratorTile() {
         super(VEBlocks.PRIMITIVE_STIRLING_GENERATOR_TILE);
@@ -29,29 +34,44 @@ public class PrimitiveStirlingGeneratorTile extends TileEntity implements ITicka
     @Override
     public void read(CompoundNBT tag) {
         CompoundNBT inv = tag.getCompound("inv");
-        getHandler().deserializeNBT(inv);
+        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>)h).deserializeNBT(inv));
+        createHandler().deserializeNBT(inv);
         super.read(tag);
     }
 
     @Override
     public CompoundNBT write(CompoundNBT tag) {
-        CompoundNBT compound = getHandler().serializeNBT();
-        tag.put("inv",compound);
+        handler.ifPresent(h -> {
+            CompoundNBT compound = ((INBTSerializable<CompoundNBT>)h).serializeNBT();
+            tag.put("inv",compound);
+        });
         return super.write(tag);
     }
 
-    private ItemStackHandler getHandler() {
-        if(handler == null) {
-            handler = new ItemStackHandler(1);
-        }
-        return handler;
+    private ItemStackHandler createHandler() {
+            return new ItemStackHandler(1) {
+                @Override
+                public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                    return stack.getItem() == Items.DIAMOND;
+                }
+
+                @Nonnull
+                @Override
+                public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate)
+                {
+                    if(stack.getItem() != Items.DIAMOND) {
+                        return stack;
+                    }
+                    return super.insertItem(slot, stack, simulate);
+                }
+            };
     }
 
     @Nonnull
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
         if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return LazyOptional.of(()-> (T) getHandler());
+            return handler.cast();
         }
         return super.getCapability(cap, side);
     }
