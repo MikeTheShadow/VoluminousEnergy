806c028d55f8e2f4d89826f29193c255f72d7a93
Eelt <davidbuddy9@gmail.com>
2021-12-08 19:39:34 -0500

    Early Multitool textures and en_us.json names

--- src/main/java/com/veteam/voluminousenergy/items/tools/multitool/VEMultitools.java
+++ src/main/java/com/veteam/voluminousenergy/items/tools/multitool/VEMultitools.java
@@ -13,9 +13,9 @@ public class VEMultitools {
     public static Multitool EMPTY_MULTITOOL = new CombustionMultitool(null, "empty_multitool", MULTITOOL_PROPERTIES);
     public static Multitool IRON_DRILL_MULTITOOL = new CombustionMultitool(VEMultitoolBits.IRON_DRILL_BIT, "iron_drill_multitool", MULTITOOL_PROPERTIES);
     public static Multitool DIAMOND_DRILL_MULTITOOL = new Multitool(VEMultitoolBits.DIAMOND_DRILL_BIT, "diamond_drill_multitool", MULTITOOL_PROPERTIES);
-    public static Multitool IRON_CHAIN_MULTITOOL = new Multitool(VEMultitoolBits.IRON_CHAIN_BIT, "iron_chain_multitool", MULTITOOL_PROPERTIES);
-    public static Multitool IRON_SCOOPER_MULTITOOL = new Multitool(VEMultitoolBits.IRON_SCOOPER_BIT, "iron_scooper_multitool", MULTITOOL_PROPERTIES);
-    public static Multitool IRON_TRIMMER_MULTITOOL = new Multitool(VEMultitoolBits.IRON_TRIMMER_BIT, "iron_trimmer_multitool", MULTITOOL_PROPERTIES);
+    public static Multitool IRON_CHAIN_MULTITOOL = new CombustionMultitool(VEMultitoolBits.IRON_CHAIN_BIT, "iron_chain_multitool", MULTITOOL_PROPERTIES);
+    public static Multitool IRON_SCOOPER_MULTITOOL = new CombustionMultitool(VEMultitoolBits.IRON_SCOOPER_BIT, "iron_scooper_multitool", MULTITOOL_PROPERTIES);
+    public static Multitool IRON_TRIMMER_MULTITOOL = new CombustionMultitool(VEMultitoolBits.IRON_TRIMMER_BIT, "iron_trimmer_multitool", MULTITOOL_PROPERTIES);
 
     public static BitItem IRON_DRILL_BIT = new BitItem(VEMultitoolBits.IRON_DRILL_BIT, "iron_drill_bit", MULTITOOL_PROPERTIES);
     public static BitItem DIAMOND_DRILL_BIT = new BitItem(VEMultitoolBits.DIAMOND_DRILL_BIT, "diamond_drill_bit", MULTITOOL_PROPERTIES);
