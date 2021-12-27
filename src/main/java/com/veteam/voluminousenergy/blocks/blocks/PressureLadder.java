3102783ee84739124390f2aafaa7104611ac450d
Eelt <davidbuddy9@gmail.com>
2021-12-27 16:55:27 -0500

    Fix PressureLadder functionality

--- src/main/java/com/veteam/voluminousenergy/blocks/blocks/PressureLadder.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/blocks/PressureLadder.java
@@ -8,6 +8,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -52,6 +53,11 @@ public class PressureLadder extends LadderBlock {
         stateBuilder.add(FACING, WATERLOGGED, POWERED);
     }
 
+    @Override
+    public boolean isLadder(BlockState blockState, LevelReader levelReader, BlockPos blockPos, LivingEntity livingEntity){
+        return true;
+    }
+
     // Pressure plate methods
     protected int getPressedTime() {
         return 20;
@@ -61,108 +67,41 @@ public class PressureLadder extends LadderBlock {
         return true;
     }
 
-    public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, Random random) {
-        int i = this.getSignalForState(blockState);
-        if (i > 0) {
-            this.checkPressed(null, serverLevel, blockPos, blockState, i);
-        }
-
+    protected int getSignalForState(BlockState blockState) {
+        return blockState.getValue(POWERED) ? 15 : 0;
     }
 
-    public void entityInside(BlockState blockState, Level world, BlockPos blockPos, Entity entity) {
-        if (!world.isClientSide) {
-            int i = this.getSignalForState(blockState);
-            if (i == 0) {
-                this.checkPressed(entity, world, blockPos, blockState, i);
-            }
-        }
-        super.entityInside(blockState, world, blockPos, entity);
+    protected BlockState setSignalForState(BlockState blockState, int signalPower) {
+        return blockState.setValue(POWERED, Boolean.valueOf(signalPower > 0));
     }
 
-    protected void checkPressed(@Nullable Entity entity, Level world, BlockPos blockPos, BlockState blockState, int num) {
-        int i = this.getSignalStrength(world, blockPos);
-        boolean flag = num > 0;
-        boolean flag1 = i > 0;
-        if (num != i) {
-            BlockState blockstate = this.setSignalForState(blockState, i);
-            world.setBlock(blockPos, blockstate, 2);
-            this.updateNeighbours(world, blockPos);
-            world.setBlocksDirty(blockPos, blockState, blockstate);
-        }
-
-        if (!flag1 && flag) {
-            this.playOffSound(world, blockPos);
-            world.gameEvent(entity, GameEvent.BLOCK_UNPRESS, blockPos);
-        } else if (flag1 && !flag) {
-            this.playOnSound(world, blockPos);
-            world.gameEvent(entity, GameEvent.BLOCK_PRESS, blockPos);
-        }
-
-        if (flag1) {
-            world.getBlockTicks().hasScheduledTick(new BlockPos(blockPos),this);
-            //world.getBlockTicks().scheduleTick(new BlockPos(blockPos), this, this.getPressedTime());
-        }
-
-    }
-
-    protected void playOnSound(LevelAccessor accessor, BlockPos blockPos) {
+    protected void playOnSound(LevelAccessor accessor, BlockPos pos) {
         if (this.material != Material.WOOD && this.material != Material.NETHER_WOOD) {
-            accessor.playSound(null, blockPos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 0.3F, 0.6F);
+            accessor.playSound((Player)null, pos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 0.3F, 0.6F);
         } else {
-            accessor.playSound(null, blockPos, SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 0.3F, 0.8F);
+            accessor.playSound((Player)null, pos, SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 0.3F, 0.8F);
         }
 
     }
 
-    protected void playOffSound(LevelAccessor accessor, BlockPos blockPos) {
+    protected void playOffSound(LevelAccessor accessor, BlockPos pos) {
         if (this.material != Material.WOOD && this.material != Material.NETHER_WOOD) {
-            accessor.playSound(null, blockPos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_OFF, SoundSource.BLOCKS, 0.3F, 0.5F);
+            accessor.playSound((Player)null, pos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_OFF, SoundSource.BLOCKS, 0.3F, 0.5F);
         } else {
-            accessor.playSound(null, blockPos, SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_OFF, SoundSource.BLOCKS, 0.3F, 0.7F);
+            accessor.playSound((Player)null, pos, SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_OFF, SoundSource.BLOCKS, 0.3F, 0.7F);
         }
 
     }
 
-    public void onRemove(BlockState blockState, Level world, BlockPos blockPos, BlockState blockState1, boolean b) {
-        if (!b && !blockState.is(blockState1.getBlock())) {
-            if (this.getSignalForState(blockState) > 0) {
-                this.updateNeighbours(world, blockPos);
-            }
-
-            super.onRemove(blockState, world, blockPos, blockState1, b);
-        }
-    }
-
-    protected void updateNeighbours(Level world, BlockPos blockPos) {
-        world.updateNeighborsAt(blockPos, this);
-        world.updateNeighborsAt(blockPos.below(), this);
-    }
-
-    public int getSignal(BlockState p_49309_, BlockGetter p_49310_, BlockPos p_49311_, Direction p_49312_) {
-        return this.getSignalForState(p_49309_);
-    }
-
-    public int getDirectSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction) {
-        return direction == Direction.UP ? this.getSignalForState(blockState) : 0;
-    }
-
-    public boolean isSignalSource(BlockState blockState) {
-        return true;
-    }
-
-    public PushReaction getPistonPushReaction(BlockState blockState) {
-        return PushReaction.DESTROY;
-    }
-
-    protected int getSignalStrength(Level world, BlockPos blockPos) {
+    protected int getSignalStrength(Level level, BlockPos blockPos) {
         AABB aabb = TOUCH_AABB.move(blockPos);
         List<? extends Entity> list;
         switch(this.sensitivity) {
             case EVERYTHING:
-                list = world.getEntities(null, aabb);
+                list = level.getEntities((Entity)null, aabb);
                 break;
             case MOBS:
-                list = world.getEntitiesOfClass(LivingEntity.class, aabb);
+                list = level.getEntitiesOfClass(LivingEntity.class, aabb);
                 break;
             default:
                 return 0;
@@ -179,16 +118,86 @@ public class PressureLadder extends LadderBlock {
         return 0;
     }
 
-    protected int getSignalForState(BlockState blockState) {
-        return blockState.getValue(POWERED) ? 15 : 0;
+    public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, Random random) {
+        int i = this.getSignalForState(blockState);
+        if (i > 0) {
+            this.checkPressed((Entity)null, serverLevel, blockPos, blockState, i);
+        }
     }
 
-    protected BlockState setSignalForState(BlockState blockState, int signalStrength) {
-        return blockState.setValue(POWERED, Boolean.valueOf(signalStrength > 0));
+    public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity) {
+        if (!level.isClientSide) {
+            int i = this.getSignalForState(blockState);
+            if (i == 0) {
+                this.checkPressed(entity, level, blockPos, blockState, i);
+            }
+
+        }
+    }
+
+    protected void checkPressed(@Nullable Entity entity, Level level, BlockPos blockPos, BlockState blockState, int iFlag) {
+        int i = this.getSignalStrength(level, blockPos);
+        boolean flag = iFlag > 0;
+        boolean flag1 = i > 0;
+        if (iFlag != i) {
+            BlockState blockstate = this.setSignalForState(blockState, i);
+            level.setBlock(blockPos, blockstate, 2);
+            this.updateNeighbours(level, blockPos);
+            level.setBlocksDirty(blockPos, blockState, blockstate);
+        }
+
+        if (!flag1 && flag) {
+            this.playOffSound(level, blockPos);
+            level.gameEvent(entity, GameEvent.BLOCK_UNPRESS, blockPos);
+        } else if (flag1 && !flag) {
+            this.playOnSound(level, blockPos);
+            level.gameEvent(entity, GameEvent.BLOCK_PRESS, blockPos);
+        }
+
+        if (flag1) {
+            level.scheduleTick(new BlockPos(blockPos), this, this.getPressedTime());
+        }
+
+    }
+
+    public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState1, boolean flag) {
+        if (!flag && !blockState.is(blockState1.getBlock())) {
+            if (this.getSignalForState(blockState) > 0) {
+                this.updateNeighbours(level, blockPos);
+            }
+
+            super.onRemove(blockState, level, blockPos, blockState1, flag);
+        }
+    }
+
+    protected void updateNeighbours(Level level, BlockPos pos) {
+        level.updateNeighborsAt(pos, this);
+        level.updateNeighborsAt(pos.below(), this);
+        level.updateNeighborsAt(pos.above(), this);
+        level.updateNeighborsAt(pos.east(), this);
+        level.updateNeighborsAt(pos.west(), this);
+        level.updateNeighborsAt(pos.north(), this);
+        level.updateNeighborsAt(pos.south(), this);
     }
 
     @Override
-    public boolean isLadder(BlockState blockState, LevelReader levelReader, BlockPos blockPos, LivingEntity livingEntity){
+    public int getSignal(BlockState blockState, BlockGetter blockGetter, BlockPos pos, Direction direction) {
+        return this.getSignalForState(blockState);
+    }
+
+    @Override
+    public int getDirectSignal(BlockState blockState, BlockGetter blockGetter, BlockPos pos, Direction direction) {
+        return direction == Direction.UP ? this.getSignalForState(blockState) : 0;
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState blockState) {
         return true;
     }
+
+    @Override
+    public PushReaction getPistonPushReaction(BlockState blockState) {
+        return PushReaction.DESTROY;
+    }
+
 }
