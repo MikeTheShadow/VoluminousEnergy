e6bb4aa2ed86295beee653b2975b455a4543f86c
gisellevonbingen <gisellevonbingen@gmail.com>
2022-03-01 13:45:37 +0900

    fix whitespace in GuiContainerHandler

--- src/main/java/com/veteam/voluminousenergy/compat/jei/PrimitiveStirlingGeneratorContainerHandler.java
+++ src/main/java/com/veteam/voluminousenergy/compat/jei/PrimitiveStirlingGeneratorContainerHandler.java
@@ -15,29 +15,29 @@ import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.network.chat.Component;
 
 public class PrimitiveStirlingGeneratorContainerHandler implements IGuiContainerHandler<PrimitiveStirlingGeneratorScreen> {
-	@Override
-	public Collection<IGuiClickableArea> getGuiClickableAreas(PrimitiveStirlingGeneratorScreen containerScreen, double guiMouseX, double guiMouseY) {
-		List<IGuiClickableArea> areas = new ArrayList<>();
-		areas.add(new IGuiClickableArea() {
-			@Override
-			public Rect2i getArea() {
-				return new Rect2i(79, 53, 18, 18);
-			}
+    @Override
+    public Collection<IGuiClickableArea> getGuiClickableAreas(PrimitiveStirlingGeneratorScreen containerScreen, double guiMouseX, double guiMouseY) {
+        List<IGuiClickableArea> areas = new ArrayList<>();
+        areas.add(new IGuiClickableArea() {
+            @Override
+            public Rect2i getArea() {
+                return new Rect2i(79, 53, 18, 18);
+            }
 
-			@Override
-			public List<Component> getTooltipStrings() {
-				List<Component> tooltips = new ArrayList<>();
-				tooltips.add(VoluminousEnergyPlugin.SHOW_RECIPES);
-				tooltips.addAll(containerScreen.getTooltips());
-				return tooltips;
-			}
+            @Override
+            public List<Component> getTooltipStrings() {
+                List<Component> tooltips = new ArrayList<>();
+                tooltips.add(VoluminousEnergyPlugin.SHOW_RECIPES);
+                tooltips.addAll(containerScreen.getTooltips());
+                return tooltips;
+            }
 
-			@Override
-			public void onClick(IFocusFactory focusFactory, IRecipesGui recipesGui) {
-				recipesGui.showCategories(Lists.newArrayList(VoluminousEnergyPlugin.STIRLING_UID));
-			}
-		});
+            @Override
+            public void onClick(IFocusFactory focusFactory, IRecipesGui recipesGui) {
+                recipesGui.showCategories(Lists.newArrayList(VoluminousEnergyPlugin.STIRLING_UID));
+            }
+        });
 
-		return areas;
-	}
+        return areas;
+    }
 }
