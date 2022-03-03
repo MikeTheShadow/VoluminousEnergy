fca445253ada40070b6fd85fccb895937f86cddd
Eelt <davidbuddy9@gmail.com>
2022-03-03 18:15:09 -0500

    Update Forge, JEI, and TagUtil

--- src/main/java/com/veteam/voluminousenergy/util/TagUtil.java
+++ src/main/java/com/veteam/voluminousenergy/util/TagUtil.java
@@ -1,16 +1,58 @@
 package com.veteam.voluminousenergy.util;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.Tag;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.ForgeTagHandler;
-import net.minecraftforge.common.Tags;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.material.Fluid;
 
-import java.util.function.Function;
+import java.util.ArrayList;
 
-public class TagUtil {/* TODO: Delete Possibly?
+public class TagUtil {
+
+    public static ArrayList<Fluid> getFluidListFromTagResourceLocation(String fluidTagLocation){
+        TagKey<Fluid> fluidTagKey = TagKey.create(Registry.FLUID_REGISTRY, new ResourceLocation(fluidTagLocation));
+        ArrayList<Fluid> fluids = new ArrayList<>();
+
+        for(Holder<Fluid> holder : Registry.FLUID.getTagOrEmpty(fluidTagKey)) {
+            fluids.add(holder.value());
+        }
+        return fluids;
+    }
+
+    public static ArrayList<Fluid> getFluidListFromTagResourceLocation(ResourceLocation fluidTagLocation){
+        TagKey<Fluid> fluidTagKey = TagKey.create(Registry.FLUID_REGISTRY, fluidTagLocation);
+        ArrayList<Fluid> fluids = new ArrayList<>();
+
+        for(Holder<Fluid> holder : Registry.FLUID.getTagOrEmpty(fluidTagKey)) {
+            fluids.add(holder.value());
+        }
+        return fluids;
+    }
+
+    public static ArrayList<Item> getItemListFromTagResourceLocation(String itemTagLocation){
+        TagKey<Item> itemTagKey = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(itemTagLocation));
+        ArrayList<Item> items = new ArrayList<>();
+
+        for(Holder<Item> holder : Registry.ITEM.getTagOrEmpty(itemTagKey)) {
+            items.add(holder.value());
+        }
+        return items;
+    }
+
+    public static ArrayList<Item> getItemListFromTagResourceLocation(ResourceLocation itemTagLocation){
+        TagKey<Item> itemTagKey = TagKey.create(Registry.ITEM_REGISTRY, itemTagLocation);
+        ArrayList<Item> items = new ArrayList<>();
+
+        for(Holder<Item> holder : Registry.ITEM.getTagOrEmpty(itemTagKey)) {
+            items.add(holder.value());
+        }
+        return items;
+    }
+
+    /*
+
     // USE: TagKey.create(Registry.<TYPE>_REGISTRY, resourceLocation);
 
     public static Tag.Named<Block> getStaticBlockTagFromResourceLocation(String blockTagLocation){ // Doesn't work
