f3ac9c279aa16f8a5c06ad4543aeea6bbcaacab7
Eelt <davidbuddy9@gmail.com>
2022-03-17 09:31:00 -0400

    Port CentrifugalAgitationCategory

--- src/main/java/com/veteam/voluminousenergy/compat/jei/category/CentrifugalAgitationCategory.java
+++ src/main/java/com/veteam/voluminousenergy/compat/jei/category/CentrifugalAgitationCategory.java
@@ -3,25 +3,27 @@ package com.veteam.voluminousenergy.compat.jei.category;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.veteam.voluminousenergy.VoluminousEnergy;
 import com.veteam.voluminousenergy.blocks.blocks.VEBlocks;
+import com.veteam.voluminousenergy.blocks.screens.VEContainerScreen;
 import com.veteam.voluminousenergy.compat.jei.VoluminousEnergyPlugin;
 import com.veteam.voluminousenergy.recipe.CentrifugalAgitatorRecipe;
 import com.veteam.voluminousenergy.util.TextUtil;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.builder.IIngredientAcceptor;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
-import mezz.jei.api.gui.ingredient.IGuiFluidStackGroup;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fluids.FluidStack;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class CentrifugalAgitationCategory implements IRecipeCategory<CentrifugalAgitatorRecipe> {
     private final IDrawable background;
@@ -34,17 +36,24 @@ public class CentrifugalAgitationCategory implements IRecipeCategory<Centrifugal
         // 68, 12 | 40, 65 -> 10 px added for chance
         ResourceLocation GUI = new ResourceLocation(VoluminousEnergy.MODID, "textures/gui/jei/jei.png");
         background = guiHelper.drawableBuilder(GUI, 68, 12, 90, 40).build();
-        icon = guiHelper.createDrawableIngredient(new ItemStack(VEBlocks.CENTRIFUGAL_AGITATOR_BLOCK));
+        icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM, new ItemStack(VEBlocks.CENTRIFUGAL_AGITATOR_BLOCK));
         slotDrawable = guiHelper.getSlotDrawable();
         arrow = guiHelper.drawableBuilder(GUI, 176, 0, 23, 17).build();
         emptyArrow = guiHelper.drawableBuilder(GUI,199,0,23,17).buildAnimated(200, IDrawableAnimated.StartDirection.LEFT, true);
     }
 
     @Override
+    public @NotNull RecipeType getRecipeType(){
+        return new RecipeType(VoluminousEnergyPlugin.CENTRIFUGAL_AGITATION_UID, CentrifugalAgitatorRecipe.class);
+    }
+
+    @Deprecated
+    @Override
     public ResourceLocation getUid(){
         return VoluminousEnergyPlugin.CENTRIFUGAL_AGITATION_UID;
     }
 
+    @Deprecated
     @Override
     public Class<? extends CentrifugalAgitatorRecipe> getRecipeClass() {
         return CentrifugalAgitatorRecipe.class;
@@ -66,44 +75,43 @@ public class CentrifugalAgitationCategory implements IRecipeCategory<Centrifugal
     }
 
     @Override
-    public void draw(CentrifugalAgitatorRecipe recipe, PoseStack matrixStack, double mouseX, double mouseY) {
+    public void draw(CentrifugalAgitatorRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack matrixStack, double mouseX, double mouseY) {
         arrow.draw(matrixStack,24, 12);
         emptyArrow.draw(matrixStack,24,12);
         slotDrawable.draw(matrixStack,2,10);
         slotDrawable.draw(matrixStack,48,10);
         slotDrawable.draw(matrixStack,72,10);
 
-        Minecraft.getInstance().font.draw(matrixStack,"mB:", -20,32, 0x606060);
-        Minecraft.getInstance().font.draw(matrixStack,recipe.getInputAmount() + "", 2, 32,0x606060);
-        Minecraft.getInstance().font.draw(matrixStack,recipe.getOutputAmount() + "", 48, 32,0x606060);
-        Minecraft.getInstance().font.draw(matrixStack,recipe.getSecondAmount() + "", 72, 32,0x606060);
+        Minecraft.getInstance().font.draw(matrixStack,"mB:", -20,32, VEContainerScreen.GREY_TEXT_COLOUR);
+        Minecraft.getInstance().font.draw(matrixStack,recipe.getInputAmount() + "", 2, 32,VEContainerScreen.GREY_TEXT_COLOUR);
+        Minecraft.getInstance().font.draw(matrixStack,recipe.getOutputAmount() + "", 48, 32,VEContainerScreen.GREY_TEXT_COLOUR);
+        Minecraft.getInstance().font.draw(matrixStack,recipe.getSecondAmount() + "", 72, 32,VEContainerScreen.GREY_TEXT_COLOUR);
+    }
+
+    public void ingredientHandler(CentrifugalAgitatorRecipe recipe,
+                                  IIngredientAcceptor fluidInputAcceptor,
+                                  IIngredientAcceptor firstFluidOutputAcceptor,
+                                  IIngredientAcceptor secondFluidOutputAcceptor) {
+
+        fluidInputAcceptor.addIngredients(VanillaTypes.FLUID, recipe.fluidInputList.get());
+
+        firstFluidOutputAcceptor.addIngredient(VanillaTypes.FLUID, recipe.getOutputFluid());
+        secondFluidOutputAcceptor.addIngredient(VanillaTypes.FLUID, recipe.getSecondFluid());
     }
 
     @Override
-    public void setIngredients(CentrifugalAgitatorRecipe recipe, IIngredients ingredients) {
-
-        // INPUT
-        ingredients.setInputs(VanillaTypes.FLUID, recipe.fluidInputList.get());
-
-        // OUTPUT
-        List<FluidStack> outputStacks = new ArrayList<>();
-        outputStacks.add(recipe.getOutputFluid()); // Normal output
-        outputStacks.add(recipe.getSecondFluid()); // Second output
-        ingredients.setOutputs(VanillaTypes.FLUID, outputStacks);
-    }
-
-    @Override
-    public void setRecipe(IRecipeLayout recipeLayout, CentrifugalAgitatorRecipe recipe, IIngredients ingredients) {
-        IGuiFluidStackGroup fluidStacks = recipeLayout.getFluidStacks();
-        fluidStacks.init(0, true, 3, 11);
-        fluidStacks.init(1, false, 49, 11);
-        fluidStacks.init(2, false, 73,11);
-
+    public void setRecipe(IRecipeLayoutBuilder recipeLayout, CentrifugalAgitatorRecipe recipe, IFocusGroup focusGroup) {
         // Input
-        fluidStacks.set(0, recipe.fluidInputList.get());
+        IRecipeSlotBuilder fluidInput = recipeLayout.addSlot(RecipeIngredientRole.INPUT, 3, 11);
 
-        // Calculate output
-        fluidStacks.set(1, recipe.getOutputFluid());
-        fluidStacks.set(2, recipe.getSecondFluid());
+        // Output
+        IRecipeSlotBuilder firstFluidOutput = recipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 49, 11);
+        IRecipeSlotBuilder secondFluidOutput = recipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 73, 11);
+
+        fluidInput.setSlotName(TextUtil.TRANSLATED_INPUT_TANK.getString());
+        firstFluidOutput.setSlotName(TextUtil.TRANSLATED_OUTPUT_TANK.getString());
+        secondFluidOutput.setSlotName(TextUtil.TRANSLATED_OUTPUT_TANK.getString());
+
+        this.ingredientHandler(recipe, fluidInput, firstFluidOutput, secondFluidOutput);
     }
 }
