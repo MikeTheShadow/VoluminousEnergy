75dd31a9c2b3951fbc6098ebb64c3d0867c88af0
Eelt <davidbuddy9@gmail.com>
2022-03-17 20:02:50 -0400

    Port DistillationRecipe

--- src/main/java/com/veteam/voluminousenergy/compat/jei/category/DistillingCategory.java
+++ src/main/java/com/veteam/voluminousenergy/compat/jei/category/DistillingCategory.java
@@ -3,26 +3,27 @@ package com.veteam.voluminousenergy.compat.jei.category;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.veteam.voluminousenergy.VoluminousEnergy;
 import com.veteam.voluminousenergy.blocks.blocks.VEBlocks;
+import com.veteam.voluminousenergy.blocks.screens.VEContainerScreen;
 import com.veteam.voluminousenergy.compat.jei.VoluminousEnergyPlugin;
 import com.veteam.voluminousenergy.recipe.DistillationRecipe;
 import com.veteam.voluminousenergy.util.TextUtil;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.builder.IIngredientAcceptor;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
-import mezz.jei.api.gui.ingredient.IGuiFluidStackGroup;
-import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fluids.FluidStack;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class DistillingCategory implements IRecipeCategory<DistillationRecipe> {
     private final IDrawable background;
@@ -35,17 +36,24 @@ public class DistillingCategory implements IRecipeCategory<DistillationRecipe> {
         // 68, 12 | 40, 65 -> 10 px added for chance
         ResourceLocation GUI = new ResourceLocation(VoluminousEnergy.MODID, "textures/gui/jei/jei.png");
         background = guiHelper.drawableBuilder(GUI, 42, 5, 128, 40).build();
-        icon = guiHelper.createDrawableIngredient(new ItemStack(VEBlocks.DISTILLATION_UNIT_BLOCK));
+        icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM, new ItemStack(VEBlocks.DISTILLATION_UNIT_BLOCK));
         slotDrawable = guiHelper.getSlotDrawable();
         arrow = guiHelper.drawableBuilder(GUI, 176, 0, 23, 17).build();
         emptyArrow = guiHelper.drawableBuilder(GUI,199,0,23,17).buildAnimated(200, IDrawableAnimated.StartDirection.LEFT, true);
     }
 
     @Override
+    public @NotNull RecipeType getRecipeType(){
+        return new RecipeType(VoluminousEnergyPlugin.DISTILLING_UID, DistillationRecipe.class);
+    }
+
+    @Deprecated
+    @Override
     public ResourceLocation getUid(){
         return VoluminousEnergyPlugin.DISTILLING_UID;
     }
 
+    @Deprecated
     @Override
     public Class<? extends DistillationRecipe> getRecipeClass() {
         return DistillationRecipe.class;
@@ -67,7 +75,7 @@ public class DistillingCategory implements IRecipeCategory<DistillationRecipe> {
     }
 
     @Override
-    public void draw(DistillationRecipe recipe, PoseStack matrixStack, double mouseX, double mouseY) {
+    public void draw(DistillationRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack matrixStack, double mouseX, double mouseY) {
         arrow.draw(matrixStack,24, 12);
         emptyArrow.draw(matrixStack,24,12);
         slotDrawable.draw(matrixStack,2,10);
@@ -75,42 +83,44 @@ public class DistillingCategory implements IRecipeCategory<DistillationRecipe> {
         slotDrawable.draw(matrixStack,72,10);
         slotDrawable.draw(matrixStack,96,10);
 
-        Minecraft.getInstance().font.draw(matrixStack,"mB:", -20,32, 0x606060);
-        Minecraft.getInstance().font.draw(matrixStack,recipe.getInputAmount() + "", 2, 32,0x606060);
-        Minecraft.getInstance().font.draw(matrixStack,recipe.getOutputAmount() + "", 48, 32,0x606060);
-        Minecraft.getInstance().font.draw(matrixStack,recipe.getAmounts().get(2) + "", 72, 32,0x606060);
+        Minecraft.getInstance().font.draw(matrixStack,"mB:", -20,32, VEContainerScreen.GREY_TEXT_COLOUR);
+        Minecraft.getInstance().font.draw(matrixStack,recipe.getInputAmount() + "", 2, 32,VEContainerScreen.GREY_TEXT_COLOUR);
+        Minecraft.getInstance().font.draw(matrixStack,recipe.getOutputAmount() + "", 48, 32,VEContainerScreen.GREY_TEXT_COLOUR);
+        Minecraft.getInstance().font.draw(matrixStack,recipe.getAmounts().get(2) + "", 72, 32,VEContainerScreen.GREY_TEXT_COLOUR);
     }
 
-    @Override
-    public void setIngredients(DistillationRecipe recipe, IIngredients ingredients) {
-        // INPUT
-        ingredients.setInputs(VanillaTypes.FLUID, recipe.fluidInputList.get());
-
-        // OUTPUT
-        List<FluidStack> outputStacks = new ArrayList<>();
-        outputStacks.add(recipe.getOutputFluid()); // Normal output
-        outputStacks.add(recipe.getSecondFluid());
-
-        ingredients.setOutputs(VanillaTypes.FLUID, outputStacks);
-        ingredients.setOutput(VanillaTypes.ITEM, recipe.getThirdResult());
-    }
-
-    @Override
-    public void setRecipe(IRecipeLayout recipeLayout, DistillationRecipe recipe, IIngredients ingredients) {
-        IGuiItemStackGroup itemStacks = recipeLayout.getItemStacks();
-        IGuiFluidStackGroup fluidStacks = recipeLayout.getFluidStacks();
-
-        fluidStacks.init(0, true, 3,11);
-        fluidStacks.init(1, false, 49,11);
-        fluidStacks.init(2, false, 73,11);
-        itemStacks.init(3,false, 96,10);
-
+    public void ingredientHandler(DistillationRecipe recipe,
+                                  IIngredientAcceptor fluidInputAcceptor,
+                                  IIngredientAcceptor firstFluidOutputAcceptor,
+                                  IIngredientAcceptor secondFluidOutputAcceptor,
+                                  IIngredientAcceptor itemOutputAcceptor) {
         // Input
-        fluidStacks.set(0, recipe.fluidInputList.get());
+        fluidInputAcceptor.addIngredients(VanillaTypes.FLUID, recipe.fluidInputList.get());
 
-        // Calculate output
-        fluidStacks.set(1, recipe.getOutputFluid());
-        fluidStacks.set(2, recipe.getSecondFluid());
-        itemStacks.set(3, recipe.getThirdResult());
+        // Output
+        firstFluidOutputAcceptor.addIngredient(VanillaTypes.FLUID, recipe.getOutputFluid()); // seems like amount is set correctly
+        secondFluidOutputAcceptor.addIngredient(VanillaTypes.FLUID, recipe.getSecondFluid()); // seems like amount is set correctly
+
+        ItemStack itemStackResult = recipe.getThirdResult();
+        itemStackResult.setCount(recipe.getThirdAmount());
+        itemOutputAcceptor.addIngredient(VanillaTypes.ITEM,itemStackResult);
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder recipeLayout, DistillationRecipe recipe, IFocusGroup focusGroup) {
+        // Inputs
+        IRecipeSlotBuilder fluidInput = recipeLayout.addSlot(RecipeIngredientRole.INPUT, 3, 11);
+
+        // Outputs
+        IRecipeSlotBuilder firstFluidOutput = recipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 49, 11);
+        IRecipeSlotBuilder secondFluidOutput = recipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 73, 11);
+        IRecipeSlotBuilder itemOutput = recipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 97, 11);
+
+        fluidInput.setSlotName(TextUtil.TRANSLATED_INPUT_TANK.getString());
+        firstFluidOutput.setSlotName(TextUtil.TRANSLATED_OUTPUT_TANK.getString());
+        secondFluidOutput.setSlotName(TextUtil.TRANSLATED_OUTPUT_TANK.getString());
+        itemOutput.setSlotName(TextUtil.TRANSLATED_OUTPUT_SLOT.getString());
+
+        this.ingredientHandler(recipe, fluidInput, firstFluidOutput, secondFluidOutput, itemOutput);
     }
 }
