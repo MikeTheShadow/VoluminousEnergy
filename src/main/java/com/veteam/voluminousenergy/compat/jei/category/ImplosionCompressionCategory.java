2f6876e23d3999d1ee8fd8643270c26c325b8da3
Eelt <davidbuddy9@gmail.com>
2022-03-17 20:30:41 -0400

    Port ImplosionCompressionCategory

--- src/main/java/com/veteam/voluminousenergy/compat/jei/category/ImplosionCompressionCategory.java
+++ src/main/java/com/veteam/voluminousenergy/compat/jei/category/ImplosionCompressionCategory.java
@@ -7,23 +7,24 @@ import com.veteam.voluminousenergy.compat.jei.VoluminousEnergyPlugin;
 import com.veteam.voluminousenergy.recipe.ImplosionCompressorRecipe;
 import com.veteam.voluminousenergy.util.TextUtil;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.builder.IIngredientAcceptor;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
-import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ImplosionCompressionCategory implements IRecipeCategory<ImplosionCompressorRecipe> {
 
@@ -37,17 +38,24 @@ public class ImplosionCompressionCategory implements IRecipeCategory<ImplosionCo
         // 68, 12 | 40, 65 -> 10 px added for chance
         ResourceLocation GUI = new ResourceLocation(VoluminousEnergy.MODID, "textures/gui/jei/jei.png");
         background = guiHelper.drawableBuilder(GUI, 68, 12, 70, 40).build();
-        icon = guiHelper.createDrawableIngredient(new ItemStack(VEBlocks.IMPLOSION_COMPRESSOR_BLOCK));
+        icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM, new ItemStack(VEBlocks.IMPLOSION_COMPRESSOR_BLOCK));
         slotDrawable = guiHelper.getSlotDrawable();
         arrow = guiHelper.drawableBuilder(GUI, 176, 0, 23, 17).build();
         emptyArrow = guiHelper.drawableBuilder(GUI,199,0,23,17).buildAnimated(200, IDrawableAnimated.StartDirection.LEFT, true);
     }
 
     @Override
+    public @NotNull RecipeType getRecipeType(){
+        return new RecipeType(VoluminousEnergyPlugin.IMPLOSION_COMPRESSION_UID, ImplosionCompressorRecipe.class);
+    }
+
+    @Deprecated
+    @Override
     public ResourceLocation getUid(){
         return VoluminousEnergyPlugin.IMPLOSION_COMPRESSION_UID;
     }
 
+    @Deprecated
     @Override
     public Class<? extends ImplosionCompressorRecipe> getRecipeClass() {
         return ImplosionCompressorRecipe.class;
@@ -69,7 +77,7 @@ public class ImplosionCompressionCategory implements IRecipeCategory<ImplosionCo
     }
 
     @Override
-    public void draw(ImplosionCompressorRecipe recipe, PoseStack matrixStack, double mouseX, double mouseY) {
+    public void draw(ImplosionCompressorRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack matrixStack, double mouseX, double mouseY) {
         arrow.draw(matrixStack,24, 12);
         emptyArrow.draw(matrixStack,24,12);
         slotDrawable.draw(matrixStack,2,1);
@@ -77,48 +85,41 @@ public class ImplosionCompressionCategory implements IRecipeCategory<ImplosionCo
         slotDrawable.draw(matrixStack,48,10);
     }
 
-    @Override
-    public void setIngredients(ImplosionCompressorRecipe recipe, IIngredients ingredients) {
-        ingredients.setInputLists(VanillaTypes.ITEM, recipe.getIngredientMap().keySet().stream()
-                .map(ingredient -> Arrays.asList(ingredient.getItems()))
-                .collect(Collectors.toList()));
-
-        // STACK needs to be 64 for recipes that require more than 1 of the input item
-        // This for loop ensures that every input can be right clicked, maybe it can just fetch the current ingredient
-        // to save CPU cycles... but this works.
-        for (ItemStack testStack : recipe.getIngredient().getItems()){
-            testStack.setCount(64);
-            ingredients.setInput(VanillaTypes.ITEM, testStack);
+    public void ingredientHandler(ImplosionCompressorRecipe recipe,
+                                  IIngredientAcceptor itemInputAcceptor,
+                                  IIngredientAcceptor explosiveInputAcceptor,
+                                  IIngredientAcceptor itemOutputAcceptor) {
+        // Input
+        ArrayList<ItemStack> inputStacks = new ArrayList<>();
+        for (ItemStack itemStack : recipe.ingredient.get().getItems()){
+            itemStack.setCount(recipe.getIngredientCount());
+            inputStacks.add(itemStack);
         }
 
-        // OUTPUT
-        List<ItemStack> outputStacks = new ArrayList<>();
-        outputStacks.add(recipe.getResultItem()); // Normal output
+        itemInputAcceptor.addIngredients(VanillaTypes.ITEM, inputStacks);
 
-        ingredients.setOutputs(VanillaTypes.ITEM, outputStacks);
+        // Explosive input
+        explosiveInputAcceptor.addIngredient(VanillaTypes.ITEM, new ItemStack(Items.GUNPOWDER, recipe.ingredientCount));
+
+        // Output
+        ItemStack resultStack = recipe.result;
+        resultStack.setCount(recipe.getOutputAmount());
+        itemOutputAcceptor.addIngredient(VanillaTypes.ITEM, resultStack);
     }
 
     @Override
-    public void setRecipe(IRecipeLayout recipeLayout, ImplosionCompressorRecipe recipe, IIngredients ingredients) {
-        IGuiItemStackGroup itemStacks = recipeLayout.getItemStacks();
-        itemStacks.init(0, true, 2, 1);
-        itemStacks.init(2, true, 2, 19);
-        itemStacks.init(1, false, 48, 10);
+    public void setRecipe(IRecipeLayoutBuilder recipeLayout, ImplosionCompressorRecipe recipe, IFocusGroup focusGroup) {
+        // Input
+        IRecipeSlotBuilder itemInput = recipeLayout.addSlot(RecipeIngredientRole.INPUT, 3, 2);
+        IRecipeSlotBuilder explosiveInput = recipeLayout.addSlot(RecipeIngredientRole.INPUT, 3, 20);
 
-        // Should only be one ingredient...
-        List<ItemStack> inputs = new ArrayList<>();
-        Arrays.stream(recipe.getIngredient().getItems()).map(s -> {
-            ItemStack stack = s.copy();
-            stack.setCount(recipe.getIngredientCount());
-            return stack;
-        }).forEach(inputs::add);
-        itemStacks.set(0, inputs);
+        // Output
+        IRecipeSlotBuilder itemOutput = recipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 49, 11);
 
-        // Calculate output
-        ItemStack tempStack = recipe.getResultItem(); // Get Item since amount will be wrong
-        Item outputItem = tempStack.getItem();
-        ItemStack jeiStack = new ItemStack(outputItem, recipe.getOutputAmount()); // Create new stack for JEI with correct amount
-        itemStacks.set(1, jeiStack);
-        itemStacks.set(2, new ItemStack(Items.GUNPOWDER, 1));
+        itemInput.setSlotName(TextUtil.TRANSLATED_INPUT_SLOT.getString());
+        explosiveInput.setSlotName(TextUtil.TRANSLATED_INPUT_SLOT.getString());
+        itemOutput.setSlotName(TextUtil.TRANSLATED_OUTPUT_SLOT.getString());
+
+        this.ingredientHandler(recipe, itemInput, explosiveInput, itemOutput);
     }
 }
