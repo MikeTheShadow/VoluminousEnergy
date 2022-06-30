c83eeeb411bbf336b04c0f3347ecd0c1c82be74d
miketheshadow <miketheshadow1@gmail.com>
2022-06-29 23:51:37 -0600

    Prestart making VEFluidRecipe extend VERecipe

--- src/main/java/com/veteam/voluminousenergy/recipe/VEFluidRecipe.java
+++ src/main/java/com/veteam/voluminousenergy/recipe/VEFluidRecipe.java
@@ -1,15 +1,10 @@
 package com.veteam.voluminousenergy.recipe;
 
 import com.veteam.voluminousenergy.util.RecipeUtil;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.Container;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.fluids.FluidStack;
@@ -17,10 +12,8 @@ import net.minecraftforge.fluids.FluidStack;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class VEFluidRecipe implements Recipe<Container> {
+public abstract class VEFluidRecipe extends VERecipe {
 
-    public Lazy<Ingredient> ingredient;
-    public int ingredientCount;
     public ItemStack result;
 
     // Fluids
@@ -36,40 +29,9 @@ public abstract class VEFluidRecipe implements Recipe<Container> {
 
     }
 
-    public Ingredient getIngredient() {
-        return ingredient.get();
-    }
-
-    public int getIngredientCount() {
-        return ingredientCount;
-    }
-
-    public ItemStack getResult() { return result; }
-
-    @Override
-    public boolean matches(Container inv, Level worldIn){
-        ItemStack stack = inv.getItem(0);
-        int count = stack.getCount();
-        return ingredient.get().test(stack) && count >= ingredientCount;
-    }
-    @Override
-    public ItemStack assemble(Container inv){
-        return ItemStack.EMPTY;
-    }
-
     @Override
     public boolean canCraftInDimensions(int width, int height){
         return true;
-    }
-
-    @Override
-    public ItemStack getResultItem(){
-        return result;
-    }
-
-    @Override
-    public ResourceLocation getId(){
-        return null;
     }
 
     @Override
---
+++ src/main/java/com/veteam/voluminousenergy/util/RecipeCache.java
@@ -0,0 +1,10 @@
+package com.veteam.voluminousenergy.util;
+
+import com.veteam.voluminousenergy.recipe.VERecipe;
+
+public class RecipeCache<T extends VERecipe> {
+
+
+
+
+}
