111fceeab797009531d83db7bbf2772ba3c1686a
Eelt <davidbuddy9@gmail.com>
2022-08-20 19:47:47 -0400

    Update networking code

--- src/main/java/com/veteam/voluminousenergy/tools/networking/VENetwork.java
+++ src/main/java/com/veteam/voluminousenergy/tools/networking/VENetwork.java
@@ -23,43 +23,43 @@ public class VENetwork {
         channel.messageBuilder(BoolButtonPacket.class, 1)
                 .decoder(BoolButtonPacket::fromBytes)
                 .encoder(BoolButtonPacket::toBytes)
-                .consumer(BoolButtonPacket::handle)
+                .consumerMainThread(BoolButtonPacket::handle)
                 .add();
 
         channel.messageBuilder(DirectionButtonPacket.class, 2)
                 .decoder(DirectionButtonPacket::fromBytes)
                 .encoder(DirectionButtonPacket::toBytes)
-                .consumer(DirectionButtonPacket::handle)
+                .consumerMainThread(DirectionButtonPacket::handle)
                 .add();
 
         channel.messageBuilder(UuidPacket.class, 3)
                 .decoder(UuidPacket::fromBytes)
                 .encoder(UuidPacket::toBytes)
-                .consumer(UuidPacket::handle)
+                .consumerMainThread(UuidPacket::handle)
                 .add();
 
         channel.messageBuilder(TankBoolPacket.class, 4)
                 .decoder(TankBoolPacket::fromBytes)
                 .encoder(TankBoolPacket::toBytes)
-                .consumer(TankBoolPacket::handle)
+                .consumerMainThread(TankBoolPacket::handle)
                 .add();
 
         channel.messageBuilder(TankDirectionPacket.class, 5)
                 .decoder(TankDirectionPacket::fromBytes)
                 .encoder(TankDirectionPacket::toBytes)
-                .consumer(TankDirectionPacket::handle)
+                .consumerMainThread(TankDirectionPacket::handle)
                 .add();
 
         channel.messageBuilder(BatteryBoxSlotPairPacket.class, 6)
                 .decoder(BatteryBoxSlotPairPacket::fromBytes)
                 .encoder(BatteryBoxSlotPairPacket::toBytes)
-                .consumer(BatteryBoxSlotPairPacket::handle)
+                .consumerMainThread(BatteryBoxSlotPairPacket::handle)
                 .add();
 
         channel.messageBuilder(BatteryBoxSendOutPowerPacket.class, 7)
                 .decoder(BatteryBoxSendOutPowerPacket::fromBytes)
                 .encoder(BatteryBoxSendOutPowerPacket::toBytes)
-                .consumer(BatteryBoxSendOutPowerPacket::handle)
+                .consumerMainThread(BatteryBoxSendOutPowerPacket::handle)
                 .add();
     }
 
