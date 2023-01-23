b690c8283be067b68d72412a9c8c2c4c52f9c5de
Eelt <davidbuddy9@gmail.com>
2023-01-22 19:16:46 -0500

    Fixed a bug causing Fuming Acids to not have particles

--- src/main/java/com/veteam/voluminousenergy/fluids/flowingFluidBlocks/FumingAcidFlowingFluidBlock.java
+++ src/main/java/com/veteam/voluminousenergy/fluids/flowingFluidBlocks/FumingAcidFlowingFluidBlock.java
@@ -1,17 +1,15 @@
 package com.veteam.voluminousenergy.fluids.flowingFluidBlocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FlowingFluid;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-import java.util.Random;
 import java.util.function.Supplier;
-
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 
 public class FumingAcidFlowingFluidBlock extends AcidFlowingFluidBlock {
     public FumingAcidFlowingFluidBlock(Supplier<? extends FlowingFluid> supplier, Properties properties) {
@@ -19,7 +17,7 @@ public class FumingAcidFlowingFluidBlock extends AcidFlowingFluidBlock {
     }
 
     @OnlyIn(Dist.CLIENT)
-    public void animateTick(BlockState stateIn, Level worldIn, BlockPos pos, Random random) {
+    public void animateTick(BlockState stateIn, Level worldIn, BlockPos pos, RandomSource random) {
         if (random.nextInt(5) == 0) {
             worldIn.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, true, (double)pos.getX() + 0.5D + random.nextDouble() / 3.0D * (double)(random.nextBoolean() ? 1 : -1), (double)pos.getY() + random.nextDouble() + random.nextDouble(), (double)pos.getZ() + 0.5D + random.nextDouble() / 3.0D * (double)(random.nextBoolean() ? 1 : -1), 0.0D, 0.07D, 0.0D);
         }
