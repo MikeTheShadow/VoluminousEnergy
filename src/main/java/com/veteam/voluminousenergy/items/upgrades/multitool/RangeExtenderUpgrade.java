7fdd9de0d3faa8b728f28fdf81ecf464ec79dd4b
Eelt <davidbuddy9@gmail.com>
2023-06-08 11:02:41 -0400

    TreeFelling extraction and prep for DrillAOE

---
+++ src/main/java/com/veteam/voluminousenergy/items/upgrades/multitool/RangeExtenderUpgrade.java
@@ -0,0 +1,13 @@
+package com.veteam.voluminousenergy.items.upgrades.multitool;
+
+import net.minecraft.world.item.Item;
+
+public class RangeExtenderUpgrade extends Item {
+
+    public RangeExtenderUpgrade() {
+        super(new Item.Properties()
+                .stacksTo(1)
+        );
+    }
+
+}
