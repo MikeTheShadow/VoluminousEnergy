3487354bd6deb96575605ca14ce8973317ae7070
Eelt <davidbuddy9@gmail.com>
2023-10-08 21:46:57 -0400

    Update JSONs for Sawmill

--- src/main/java/com/veteam/voluminousenergy/recipe/VEFluidSawmillRecipe.java
+++ src/main/java/com/veteam/voluminousenergy/recipe/VEFluidSawmillRecipe.java
@@ -3,13 +3,19 @@ package com.veteam.voluminousenergy.recipe;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.veteam.voluminousenergy.blocks.blocks.VEBlocks;
-import com.veteam.voluminousenergy.util.recipe.FluidIngredient;
+import com.veteam.voluminousenergy.tools.Config;
 import com.veteam.voluminousenergy.util.recipe.FluidSerializerHelper;
 import com.veteam.voluminousenergy.util.recipe.VERecipeCodecs;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.item.crafting.CraftingRecipeCodecs;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
@@ -24,8 +30,8 @@ public class VEFluidSawmillRecipe extends VEFluidRecipe {
 
     }
 
-    public VEFluidSawmillRecipe(List<VERecipeCodecs.RegistryIngredient> i, List<VERecipeCodecs.RegistryFluidIngredient> fi, List<FluidStack> of, List<ItemStack> oi, int processTime, boolean isLogRecipe) {
-        super(i,fi,of,oi,processTime);
+    public VEFluidSawmillRecipe(List<VERecipeCodecs.RegistryIngredient> i, List<FluidStack> of, List<ItemStack> oi, int processTime, boolean isLogRecipe) {
+        super(i,List.of(),of,oi,processTime);
         this.isLogRecipe = isLogRecipe;
     }
 
@@ -33,7 +39,6 @@ public class VEFluidSawmillRecipe extends VEFluidRecipe {
 
         public static final Codec<VEFluidSawmillRecipe> VE_RECIPE_CODEC = RecordCodecBuilder.create((instance) -> instance.group(
                 VERecipeCodecs.VE_LAZY_INGREDIENT_CODEC.listOf().fieldOf("ingredients").forGetter((getter) -> getter.registryIngredients),
-                VERecipeCodecs.VE_FLUID_INGREDIENT_CODEC.listOf().fieldOf("fluid_ingredients").forGetter((getter) -> getter.registryFluidIngredients),
                 VERecipeCodecs.VE_OUTPUT_FLUID_CODEC.listOf().fieldOf("fluid_results").forGetter((getter) -> getter.fluidOutputList),
                 CraftingRecipeCodecs.ITEMSTACK_OBJECT_CODEC.listOf().fieldOf("item_results").forGetter((getter) -> getter.results),
                 Codec.INT.fieldOf("process_time").forGetter((getter) -> getter.processTime),
@@ -76,6 +81,24 @@ public class VEFluidSawmillRecipe extends VEFluidRecipe {
 
     public boolean isLogRecipe() {
         return isLogRecipe;
+    }
+
+    @Override
+    public List<FluidStack> getOutputFluids() {
+        if (this.isLogRecipe && Config.SAWMILL_ALLOW_NON_SAWMILL_RECIPE_LOGS_TO_BE_SAWED.get()) {
+            ResourceLocation fluidLocation = new ResourceLocation(Config.SAWMILL_FLUID_LOCATION.get());
+            Fluid outputFluid = ForgeRegistries.FLUIDS.getValue(fluidLocation);
+            return List.of(new FluidStack(outputFluid, Config.SAWMILL_FLUID_AMOUNT.get()));
+        }
+        return super.getOutputFluids();
+    }
+
+    @Override
+    public FluidStack getOutputFluid(int slot) {
+        if (this.isLogRecipe && Config.SAWMILL_ALLOW_NON_SAWMILL_RECIPE_LOGS_TO_BE_SAWED.get()) {
+            return this.getOutputFluid(0);
+        }
+        return super.getOutputFluid(slot);
     }
 
 }
