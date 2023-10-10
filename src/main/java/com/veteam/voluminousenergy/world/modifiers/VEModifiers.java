27cf2708bd8290cac35a91ea93235153e2d66027
Eelt <davidbuddy9@gmail.com>
2023-10-10 19:53:37 -0400

    Fix codec-related bug with BiomeModifiers

--- src/main/java/com/veteam/voluminousenergy/world/modifiers/VEModifiers.java
+++ src/main/java/com/veteam/voluminousenergy/world/modifiers/VEModifiers.java
@@ -31,7 +31,7 @@ public class VEModifiers {
                         Codec.STRING.fieldOf("blacklisted_biomes").forGetter(VEAndedMultiBiomeModifier::getBlacklistedBiome),
                         ConfiguredFeature.CODEC.fieldOf("feature").forGetter(VEAndedMultiBiomeModifier::getFeature),
                         Codec.BOOL.fieldOf("triangular_placement").forGetter(VEAndedMultiBiomeModifier::getIsTriangular),
-                        Codec.INT.fieldOf("amount").forGetter(VEAndedMultiBiomeModifier::getCount),
+                        Codec.INT.fieldOf("count").forGetter(VEAndedMultiBiomeModifier::getCount),
                         Codec.INT.fieldOf("bottom_anchor").forGetter(VEAndedMultiBiomeModifier::getBottomAnchor),
                         Codec.INT.fieldOf("top_anchor").forGetter(VEAndedMultiBiomeModifier::getTopAnchor),
                         Codec.INT.fieldOf("rarity").forGetter(VEAndedMultiBiomeModifier::getRarity),
