12a433cdb65684928be7383c710a81622ac6c6f5
Eelt <davidbuddy9@gmail.com>
2024-06-19 19:43:41 -0400

    VEItemToolTier

---
+++ src/main/java/com/veteam/voluminousenergy/items/tools/VEItemToolTier.java
@@ -0,0 +1,54 @@
+package com.veteam.voluminousenergy.items.tools;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Block;
+
+public class VEItemToolTier implements Tier {
+    int uses;
+    float speed;
+    float attackDamageBonus;
+    TagKey<Block> incorrectBlockTag;
+    int enchantmentValue;
+    Ingredient repairIngredient;
+
+    public VEItemToolTier(int uses, float speed, float attackDamageBonus, TagKey<Block> incorrectBlockTag, int enchantmentValue, Ingredient repairIngredient) {
+        this.uses = uses;
+        this.speed = speed;
+        this.attackDamageBonus = attackDamageBonus;
+        this.incorrectBlockTag = incorrectBlockTag;
+        this.enchantmentValue = enchantmentValue;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getUses() {
+        return this.uses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return this.speed;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return this.attackDamageBonus;
+    }
+
+    @Override
+    public TagKey<Block> getIncorrectBlocksForDrops() {
+        return this.incorrectBlockTag;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient;
+    }
+}
