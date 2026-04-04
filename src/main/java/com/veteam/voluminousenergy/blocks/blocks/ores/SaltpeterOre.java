f96276ce6a7f32912d745ea302715844b2ff7945
MikeTheShadow <michaelpape11@gmail.com>
2026-04-04 18:34:52 -0400

    Fixed an issue where saltpeter ore did not drop items or xp properly

--- src/main/java/com/veteam/voluminousenergy/blocks/blocks/ores/SaltpeterOre.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/blocks/ores/SaltpeterOre.java
@@ -9,8 +9,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.ColorRGBA;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.util.valueproviders.ConstantInt;
-import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
@@ -22,13 +20,17 @@ import net.minecraft.world.level.block.state.BlockState;
 public class SaltpeterOre extends ColoredFallingBlock {
     public SaltpeterOre() {
         super(new ColorRGBA(14406560),
-                Properties.of()
-                        .sound(SoundType.SAND)
-                        .strength(0.6f)
-                        .requiresCorrectToolForDrops()
+            Properties.of()
+                .sound(SoundType.SAND)
+                .strength(0.6f)
+                .requiresCorrectToolForDrops()
         );
         VETagDataGenerator.setRequiresShovel(this);
         VETagDataGenerator.setRequiresWoodAndBlacklistLowerTiers(this);
+    }
+
+    public SaltpeterOre(ColorRGBA colorRGBA, Properties properties) {
+        super(colorRGBA, properties);
     }
 
     public int xpOnDrop(RandomSource randomSource) {
@@ -36,20 +38,19 @@ public class SaltpeterOre extends ColoredFallingBlock {
     }
 
     @Override
-    protected void tryDropExperience(ServerLevel level, BlockPos pos, ItemStack heldItem, IntProvider amount) {
+    protected void spawnAfterBreak(BlockState state, ServerLevel level, BlockPos pos, ItemStack tool, boolean dropExperience) {
+        super.spawnAfterBreak(state, level, pos, tool, dropExperience);
+
         RegistryAccess registries = level.registryAccess();
 
-        Holder<Enchantment> silkTouch = registries.registryOrThrow(Registries.ENCHANTMENT).getHolderOrThrow(Enchantments.SILK_TOUCH);
-        if (EnchantmentHelper.getItemEnchantmentLevel(silkTouch, heldItem) > 0) {
-            return;
-        }
-        int xpOnDrop = Mth.nextInt(level.random, 1, 5);
+        int xpOnDrop = Mth.nextInt(level.getRandom(), 1, 5);
         Holder<Enchantment> fortune = registries.registryOrThrow(Registries.ENCHANTMENT).getHolderOrThrow(Enchantments.FORTUNE);
-        int fortuneLevel = EnchantmentHelper.getItemEnchantmentLevel(fortune, heldItem);
+        int fortuneLevel = EnchantmentHelper.getItemEnchantmentLevel(fortune, tool);
 
         if (fortuneLevel > 0) {
             xpOnDrop = xpOnDrop * (1 + fortuneLevel);
         }
-        super.tryDropExperience(level, pos, heldItem, ConstantInt.of(xpOnDrop));
+        this.popExperience(level, pos, xpOnDrop);
+
     }
 }
--- src/main/java/com/veteam/voluminousenergy/blocks/blocks/ores/red_sand/RedSaltpeterOre.java
+++ src/main/java/com/veteam/voluminousenergy/blocks/blocks/ores/red_sand/RedSaltpeterOre.java
@@ -1,25 +1,11 @@
 package com.veteam.voluminousenergy.blocks.blocks.ores.red_sand;
 
+import com.veteam.voluminousenergy.blocks.blocks.ores.SaltpeterOre;
 import com.veteam.voluminousenergy.datagen.VETagDataGenerator;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.ColorRGBA;
-import net.minecraft.util.Mth;
-import net.minecraft.util.RandomSource;
-import net.minecraft.util.valueproviders.ConstantInt;
-import net.minecraft.util.valueproviders.IntProvider;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.block.ColoredFallingBlock;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockState;
 
-public class RedSaltpeterOre extends ColoredFallingBlock {
+public class RedSaltpeterOre extends SaltpeterOre {
     public RedSaltpeterOre() {
         super(new ColorRGBA(-8356741),
                 Properties.of()
@@ -29,27 +15,5 @@ public class RedSaltpeterOre extends ColoredFallingBlock {
         );
         VETagDataGenerator.setRequiresShovel(this);
         VETagDataGenerator.setRequiresWoodAndBlacklistLowerTiers(this);
-    }
-
-    public int xpOnDrop(RandomSource randomSource) {
-        return Mth.nextInt(randomSource, 1, 9);
-    }
-
-    @Override
-    protected void tryDropExperience(ServerLevel level, BlockPos pos, ItemStack heldItem, IntProvider amount) {
-        RegistryAccess registries = level.registryAccess();
-
-        Holder<Enchantment> silkTouch = registries.registryOrThrow(Registries.ENCHANTMENT).getHolderOrThrow(Enchantments.SILK_TOUCH);
-        if (EnchantmentHelper.getItemEnchantmentLevel(silkTouch, heldItem) > 0) {
-            return;
-        }
-        int xpOnDrop = Mth.nextInt(level.random, 1, 5);
-        Holder<Enchantment> fortune = registries.registryOrThrow(Registries.ENCHANTMENT).getHolderOrThrow(Enchantments.FORTUNE);
-        int fortuneLevel = EnchantmentHelper.getItemEnchantmentLevel(fortune, heldItem);
-
-        if (fortuneLevel > 0) {
-            xpOnDrop = xpOnDrop * (1 + fortuneLevel);
-        }
-        super.tryDropExperience(level, pos, heldItem, ConstantInt.of(xpOnDrop));
     }
 }
